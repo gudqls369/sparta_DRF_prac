d64d8b631f0e64ad3047b7da45954e461a3258a3
정형빈 <gudqls369@gmail.com>
2022-10-28 20:30:03 +0900

    create article view

--- week2/articles/urls.py
+++ week2/articles/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 from articles import views
 urlpatterns = [
     path("index/", views.index, name="index"),
+    path("<int:article_id>/", views.article_view, name="article_view"),    
 ]
--- week2/articles/views.py
+++ week2/articles/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
+from articles import serializers
 from articles.models import Article
 from articles.serializers import ArticleSerializer
 
@@ -20,3 +22,10 @@ def index(request):
         else:
             print(serializer.errors)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'DELETE'])    
+def article_view(request, article_id):
+    if request.method == 'GET':
+        article = get_object_or_404(Article, id=article_id)
+        serializer = ArticleSerializer(article)
+    return Response(serializer.data)
