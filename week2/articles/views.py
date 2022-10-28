d44c50238148bcaad09e8c521934dac654a9cd46
정형빈 <gudqls369@gmail.com>
2022-10-28 16:45:35 +0900

    create serializers

--- week2/articles/views.py
+++ week2/articles/views.py
@@ -1,8 +1,17 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from articles.models import Article
 
 # Create your views here.
 
 @api_view(['GET', 'POST'])
 def index(request):
-    return Response("연결되었습니다!")
+    articles = Article.objects.all()
+    article = articles[0]
+    article_data = {
+        "tiltle":article.title,
+        "content":article.content,
+        "created_at":article.created_at,
+        "updated_at":article.updated_at,
+    }
+    return Response(article_data)
