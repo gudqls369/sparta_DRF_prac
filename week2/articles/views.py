b5667f52f0cb7f554e467f0f0d958588fda62a72
정형빈 <gudqls369@gmail.com>
2022-10-28 17:45:08 +0900

    use serializer

--- week2/articles/views.py
+++ week2/articles/views.py
@@ -1,17 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from articles.models import Article
+from articles.serializers import ArticleSerializer
 
 # Create your views here.
 
 @api_view(['GET', 'POST'])
 def index(request):
     articles = Article.objects.all()
-    article = articles[0]
-    article_data = {
-        "tiltle":article.title,
-        "content":article.content,
-        "created_at":article.created_at,
-        "updated_at":article.updated_at,
-    }
-    return Response(article_data)
+    serializer = ArticleSerializer(articles, many=True)
+    return Response(serializer.data)
