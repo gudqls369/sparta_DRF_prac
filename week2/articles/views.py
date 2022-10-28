e2071ee6835a99db5f8e3edfbf6724ac2f2ac587
정형빈 <gudqls369@gmail.com>
2022-10-28 20:21:58 +0900

    serializer advance

--- week2/articles/views.py
+++ week2/articles/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from articles.models import Article
@@ -7,6 +8,15 @@ from articles.serializers import ArticleSerializer
 
 @api_view(['GET', 'POST'])
 def index(request):
-    articles = Article.objects.all()
-    serializer = ArticleSerializer(articles, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ArticleSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            print(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
