271ddfaa143bb2ba0669e6615e61f60d413a62dd
정형빈 <gudqls369@gmail.com>
2022-10-31 19:52:35 +0900

    week3 frontend start

---
+++ api.js
@@ -0,0 +1,23 @@
+window.onload = ()=>{
+    console.log("로딩됨")
+}
+
+async function handleSignin() {
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    console.log(email, password)
+
+    const response = await fetch('http://127.0.0.1:8000/users/signup/', {
+        headers:{
+            'content-type':'application/json',
+        },
+        method:'POST',
+        body:{
+            "email":email,
+            "password":password
+        }
+    })
+
+    console.log(response)
+}
+
