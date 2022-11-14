bea87282386ab6814c14c2149ca218ab4ba61acd
gudqls369 <gudqls369@gmail.com>
2022-11-14 19:26:52 +0900

    Feat Ariticle update

--- week3/articles/views.py
+++ week3/articles/views.py
@@ -1,3 +1,4 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework import status, permissions
 from rest_framework.response import Response
@@ -16,21 +17,29 @@ class ArticleView(APIView):
         serializer = ArticleCreateSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user=request.user)
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ArticleDetailView(APIView):
     def get(self, request, article_id):
         article = Article.objects.get(id=article_id)
+        article = get_object_or_404(Article, id=article_id)
         serializer = ArticleSerializer(article)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def put(self, request, article_id):
-        pass
+        article = Article.objects.get(id=article_id)
+        serializer = ArticleCreateSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self, request, article_id):
         pass
+
     
 class CommentView(APIView):
     def get(self, request):
